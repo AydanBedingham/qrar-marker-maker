708a9a14304956f1b72a4fa361eba23e49d91d62
aydan.bedingham <aydan.bedingham@gmail.com>
2021-08-14 20:23:05 -0700

    removed unused vid qrar related classes

--- frontend/ar-marker/src/utils/qrar-payload-util.js
+++ frontend/ar-marker/src/utils/qrar-payload-util.js
@@ -36,44 +36,6 @@ const generateImageQrarPayload = (options) => {
   };
 };
 
-const generateVideoQrarPayload = (options) => {
-  const videoUrl = options.videoUrl;
-  const qrSize = options.qrSize;
-  const barcodeValue = options.barcodeValue;
-  const barcodeQuietZone = options.barcodeQuietZone;
-  const barcodeSize = qrSize * options.barcodeRatio;
-  const config = options.configuration;
-
-  const scalingFactor = (1 + barcodeQuietZone) * (qrSize / barcodeSize);
-
-  const applyScaling = (value) => {
-    return (value * scalingFactor).toFixed(3);
-  };
-
-  return {
-    markers: [
-      {
-        element: "a-marker",
-        type: "barcode",
-        value: barcodeValue,
-        children: [
-          {
-            element: "a-video",
-            opacity: Number(config.opacity).toFixed(3),
-            src: videoUrl,
-            position: `${applyScaling(config.position.x)} ${applyScaling(
-              config.position.y
-            )} ${applyScaling(config.position.z)}`,
-            rotation: `${config.rotation.x} ${config.rotation.y} ${config.rotation.z}`,
-            width: applyScaling(config.size.width),
-            height: applyScaling(config.size.height),
-          },
-        ],
-      },
-    ],
-  };
-};
-
 export const generateQrarPayload = (barcodeValue, options) => {
   switch (options.configuration.type) {
     case "image":
@@ -86,15 +48,6 @@ export const generateQrarPayload = (barcodeValue, options) => {
         barcodeValue: barcodeValue,
       });
 
-    case "video":
-      return generateVideoQrarPayload({
-        qrSize: Number(options.qrSize),
-        videoUrl: options.configuration.videoUrl,
-        configuration: options.configuration,
-        barcodeRatio: Number(process.env.REACT_APP_QRAR_MARKER_BARCODE_RATIO),
-        barcodeQuietZone: Number(process.env.REACT_APP_BARCODES_QUIET_ZONE),
-        barcodeValue: barcodeValue,
-      });
     default:
       throw new Error("Unrecognised Qrar Type!");
   }
